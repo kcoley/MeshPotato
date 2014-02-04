cec25cad3d29eed1a817fd63aaa44a66f2967c49
kcoley <kcoley@4798c841-ad15-4c48-af5a-522e50758f80>
2014-02-04 22:16:58 +0000

    put VDBVolumeGrid back.  Accidentally deleted before...
    git-svn-id: https://svn.cs.clemson.edu/repos/KCOLEY_MeshPotato@59 4798c841-ad15-4c48-af5a-522e50758f80

---
+++ include/MPVolume/VDBVolumeGrid.h
@@ -0,0 +1,32 @@
+#include "MPUtils/Vector.h"
+#include "MPVolume/MPVolumeGrid.h"
+#include <openvdb/Grid.h>
+#include <openvdb/tools/Interpolation.h>
+using MeshPotato::MPUtils::MPVec3;
+typedef openvdb::FloatGrid::Ptr VDBFloatGridPtr;
+namespace MeshPotato {
+	namespace MPVolume {
+		class VDBVolumeGrid: public MeshPotato::MPVolume::Volume<float> {
+			public:
+				static boost::shared_ptr<Volume<float> > Ptr(const openvdb::FloatGrid::Ptr _grid) {
+					return boost::shared_ptr<Volume<float> >(new VDBVolumeGrid(_grid)); 
+				}
+				VDBVolumeGrid(const openvdb::FloatGrid::Ptr _grid) : grid(_grid), interpolator(grid->constTree(), grid->transform()) {}
+
+				openvdb::GridBase::Ptr getVDBGrid() const { return grid; }
+				~VDBVolumeGrid() {}
+				virtual const float eval(const MPUtils::MPVec3 &P) const {
+					return -interpolator.wsSample(P);
+
+				}
+				virtual const MPVec3 grad(const MPVec3 &P) const {
+				}
+				virtual const float defaultValue() const {}
+			private:
+				openvdb::FloatGrid::Ptr grid;
+				openvdb::tools::GridSampler<openvdb::FloatTree, openvdb::tools::QuadraticSampler> interpolator;			
+
+		};
+	}
+
+}
