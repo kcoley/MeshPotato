706c25a6996dd6fd08526a1caa223a0d03967b4e
kcoley <kcoley@4798c841-ad15-4c48-af5a-522e50758f80>
2014-03-10 00:00:56 +0000

    Removed the old plugin from MeshPotato
    git-svn-id: https://svn.cs.clemson.edu/repos/KCOLEY_MeshPotato@77 4798c841-ad15-4c48-af5a-522e50758f80

--- include/MPPlugins/MeshPotatoPluginManager.h
+++
@@ -1,9 +0,0 @@
-#include <MPPlugins/meshpotatoplugin.h>
-#include <memory>
-namespace MeshPotato {
-	namespace MPPlugins {
-		class PluginManager {
-			void registerMesh(std::auto_ptr<InputMeshFactory> IMF);
-		};
-	}
-}
--- src/MeshPotatoPluginManager.C
+++
@@ -1,2 +0,0 @@
-#include <MPPlugins/MeshPotatoPluginManager.h>
-
